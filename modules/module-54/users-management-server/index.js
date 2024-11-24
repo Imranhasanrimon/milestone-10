cbab0b7a8a10fb26430e2e398984585621a5f1bc
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-24 15:15:52 +0600

    creating client side project

--- modules/module-54/users-management-server/index.js
+++ modules/module-54/users-management-server/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+    { id: 1, name: 'imran Hasan', email: 'imranhasan@gmail.com' },
+    { id: 2, name: 'abul Hasan', email: 'abulhasan@gmail.com' },
+    { id: 3, name: 'rimon Hasan', email: 'rimonhasan@gmail.com' }
+]
+
 app.get('/', (req, res) => {
     res.send('this will be shown in the browser');
+})
+
+app.get('/users', (req, res) => {
+    res.send(users);
 })
 
 app.listen(port, () => {
