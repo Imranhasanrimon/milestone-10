432c796e2bba6006e3629aff068b01211c19a549
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-24 16:33:23 +0600

    adding form for taking user inputs

--- modules/module-54/users-managemnet-clientSide/src/App.jsx
+++ modules/module-54/users-managemnet-clientSide/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value
+    const email = form.email.value
+    console.log(name, email);
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>users managemnet</h1>
+      <p>total users: {users.length}</p>
+      <form onSubmit={handleSubmit}>
+        <h2>create user</h2>
+        <input type="text" name="name" placeholder='Name' id="" /> <br />
+        <input type="text" name="email" placeholder='email' id="" /> <br />
+        <input type="submit" value="Submit" />
+      </form>
+
     </>
   )
 }
