8a46b301c40d08228e9876556e32d0ede677dd71
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-27 14:19:10 +0600

    server side is built

---
+++ modules/module-54/practice-server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5500;
+
+const users = [
+    { id: 1, name: 'imran', age: 23 },
+    { id: 2, name: 'rimon', age: 26 },
+    { id: 3, name: 'hasan', age: 30 },
+]
+
+app.get("/", (req, res) => {
+    res.send('this is root path')
+})
+
+app.get("/users", (req, res) => {
+    res.send(users)
+})
+
+app.listen(port, () => {
+    console.log('server is running on port: ', port);
+})
