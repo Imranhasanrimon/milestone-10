540267187a1a7c32978f784976ae4e4325741d32
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-27 18:49:03 +0600

    adding mongodb with server

--- modules/module-54/practice-server/index.js
+++ modules/module-54/practice-server/index.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5500;
 
 //middleware
 app.use(cors());
 app.use(express.json())
-
+//module-55
+//5sys962aIJh2uaHM
 app.get("/", (req, res) => {
     res.send('this is root path')
 })
+//____________________________________________
 
+const uri = "mongodb+srv://module-55:5sys962aIJh2uaHM@cluster0.7hbnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+//____________________________________________
 
 app.listen(port, () => {
     console.log('server is running on port: ', port);
