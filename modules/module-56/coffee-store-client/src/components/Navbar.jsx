127e5b2e465a83b3a326a2d6998e727359b842be
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-28 19:36:33 +0600

    navbar is done

--- modules/module-56/coffee-store-client/src/components/Navbar.jsx
+++ modules/module-56/coffee-store-client/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
-            <h1>this is navbar</h1>
+
+            <div className="text-2xl font-semibold flex justify-between items-center max-w-60 mx-auto mt-9">
+                <Link to="/">Coffees</Link>
+                <Link to="/addCoffee">Add Coffee</Link>
+            </div>
             <Outlet></Outlet>
         </div>
     );
