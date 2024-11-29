9c1cb05d664c320ebccd9f0b7930a0ee6b6f8f0c
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-29 21:50:48 +0600

    adding handle submit function

--- modules/module-56/coffee-store-client/src/components/SignUp.jsx
+++ modules/module-56/coffee-store-client/src/components/SignUp.jsx
@@ -1,5 +1,11 @@
 
 const SignUp = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        console.log(email);
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,7 +17,7 @@ const SignUp = () => {
                     </p>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
