c9e2ea0a8d733954968f76dc60708069fa54a906
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-29 06:29:17 +0600

    styling active navlink

--- modules/module-56/coffee-store-client/src/components/Navbar.jsx
+++ modules/module-56/coffee-store-client/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
 
             <div className="text-2xl font-semibold flex justify-between items-center max-w-60 mx-auto mt-9">
-                <Link to="/">Coffees</Link>
-                <Link to="/addCoffee">Add Coffee</Link>
+                <NavLink to="/">Coffees</NavLink>
+                <NavLink to="/addCoffee">Add Coffee</NavLink>
             </div>
             <Outlet></Outlet>
         </div>
