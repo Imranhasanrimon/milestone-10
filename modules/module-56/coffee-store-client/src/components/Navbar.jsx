c452033fb7d2e6681ced325eb6ef2c322db8b820
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-29 20:55:05 +0600

    adding in navbar

--- modules/module-56/coffee-store-client/src/components/Navbar.jsx
+++ modules/module-56/coffee-store-client/src/components/Navbar.jsx
@@ -4,9 +4,11 @@ const Navbar = () => {
     return (
         <div>
 
-            <div className="text-2xl font-semibold flex justify-between items-center max-w-60 mx-auto mt-9">
+            <div className="text-2xl font-semibold flex justify-between items-center max-w-md mx-auto mt-9">
                 <NavLink to="/">Coffees</NavLink>
                 <NavLink to="/addCoffee">Add Coffee</NavLink>
+                <NavLink to="/signUp">Sign Up</NavLink>
+                <NavLink to="/signIn">Log In</NavLink>
             </div>
             <Outlet></Outlet>
         </div>
