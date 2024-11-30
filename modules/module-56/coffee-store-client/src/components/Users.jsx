1d56825a036693b563352264979786adf73ac638
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-30 10:49:56 +0600

    creating user component

--- modules/module-56/coffee-store-client/src/components/Users.jsx
+++ modules/module-56/coffee-store-client/src/components/Users.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
     const loadedUsers = useLoaderData()
+    const [users, setUsers] = useState(loadedUsers)
     return (
         <div>
-            <h1 className="text-3xl font-bold text-center mt-6">Users({loadedUsers.length})</h1>
+            <h1 className="text-3xl font-bold text-center mt-6">Users({users.length})</h1>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Created At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {users.map((user, i) => <tr key={user._id} className="hover">
+                            <th>{i + 1}</th>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.createAt}</td>
+                        </tr>)}
+                    </tbody>
+                </table>
+            </div>
 
         </div>
     );
