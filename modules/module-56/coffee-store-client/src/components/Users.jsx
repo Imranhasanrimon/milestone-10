45ec3c02a76a0f972e9f33e37dc1813a35e134c5
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-11-30 11:22:53 +0600

    delete method is intigrited

--- modules/module-56/coffee-store-client/src/components/Users.jsx
+++ modules/module-56/coffee-store-client/src/components/Users.jsx
@@ -1,9 +1,41 @@
 import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { data, useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Users = () => {
     const loadedUsers = useLoaderData()
-    const [users, setUsers] = useState(loadedUsers)
+    const [users, setUsers] = useState(loadedUsers);
+    const handleDelete = id => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/users/${id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "User has been deleted.",
+                                icon: "success"
+                            });
+                            const remaining = users.filter(user => user._id !== id);
+                            setUsers(remaining)
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div>
             <h1 className="text-3xl font-bold text-center mt-6">Users({users.length})</h1>
@@ -17,6 +49,7 @@ const Users = () => {
                             <th>Name</th>
                             <th>Email</th>
                             <th>Created At</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -26,6 +59,10 @@ const Users = () => {
                             <td>{user.name}</td>
                             <td>{user.email}</td>
                             <td>{user.createAt}</td>
+                            <td className="space-x-2">
+                                <button className="btn">U</button>
+                                <button onClick={() => handleDelete(user._id)} className="btn">X</button>
+                            </td>
                         </tr>)}
                     </tbody>
                 </table>
