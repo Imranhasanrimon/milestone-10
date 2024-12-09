25f0a69c0a7dc8743bfc4c2103eeb26e960413b3
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-09 21:21:34 +0600

    this is noter for router

--- modules/module-55/simple-crud-client/src/main.jsx
+++ modules/module-55/simple-crud-client/src/main.jsx
@@ -31,3 +31,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+// we can create a separate file for router management:
