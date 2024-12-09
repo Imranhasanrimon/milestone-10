96bc0bf5f4b90b8e013d8977d9e7e6a40371d009
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-09 21:31:33 +0600

    editing in main.jsx

--- modules/module-55/simple-crud-client/src/main.jsx
+++ modules/module-55/simple-crud-client/src/main.jsx
@@ -31,4 +31,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-// we can create a separate file for router management:
